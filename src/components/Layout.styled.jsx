f39a5b68cf84a403c9d9b8e9c871ed8feddf064a
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-10-29 17:55:51 +0300

    fixed-bg

--- src/components/Layout.styled.jsx
+++ src/components/Layout.styled.jsx
@@ -1,6 +1,17 @@
 import styled from '@emotion/styled';
+import background from '../Images/depositphotos_68233141-stock-illustration-contact-book-doodle-drawing-seamless.png';
 
 export const LayoutStyled = styled.div`
   display: flex;
   flex-direction: column;
+  min-height: 100vh;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(251, 255, 0, 0.863),
+      rgba(0, 238, 255, 0.89)
+    ),
+    url(${background});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
 `;
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,15 +1,6 @@
 import styled from '@emotion/styled';
-import background from '../../Images/depositphotos_68233141-stock-illustration-contact-book-doodle-drawing-seamless.png';
+
 export const HomePageBox = styled.div`
   height: 100vh;
   width: 100vw;
-  background-image: linear-gradient(
-      to bottom,
-      rgba(251, 255, 0, 0.863),
-      rgba(0, 238, 255, 0.89)
-    ),
-    url(${background});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
 `;
